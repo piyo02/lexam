bd5525c19cdd9afb9de8d07fe27cebb854f31084
piyo02 <alzidni2000@gmail.com>
2020-02-09 20:37:04 +0800

    change tab

--- application/views/templates/_user_parts/footer.php
+++ application/views/templates/_user_parts/footer.php
@@ -17,5 +17,47 @@
     <script src="<?= base_url('users-assets/') ?>js/morphext.min.js"></script>
     <script src="<?= base_url('users-assets/') ?>js/validator.min.js"></script>
     <script src="<?= base_url('users-assets/') ?>js/scripts.js"></script>
+
+
+<script>
+$(window).on('blur', function () {
+    console.log(1);
+    alert('You switched the tab');
+});
+
+// $(window).blur(function () {
+//     alert('You switched the tab');
+// });
+
+// window.onunload = popup();
+
+// window.addEventListener('beforeunload', function (e) { 
+//     e.preventDefault(); 
+//     e.returnValue = ''; 
+// }); 
+
+// window.onbeforeunload = function () {
+//     return 'Are you really want to perform the action?';
+// }
+
+// window.addEventListener('blur', popup);
+
+// window.addEventListener('beforeunload', (event) => {
+//     event.returnValue = `Are you sure you want to leave?`;
+// });
+
+// $(function () {
+//       $("#tabss").tabs({
+//             activate: function( event, ui ) {
+//                   popup;
+//             }
+//       });
+// });
+
+
+// function popup() {
+//   alert('tab changed');
+// }
+</script>
 </body>
 </html>
